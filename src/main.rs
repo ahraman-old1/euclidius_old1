0b8e083aba8a5218efe5a6685346d5af1a263b22
Ahraman <ahraman12000@gmail.com>
2024-05-15 12:00:17 +0330

    created server startup code using tokio and axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = build_router();
+    let listener = TcpListener::bind("localhost:3000").await.unwrap();
+    axum::serve(listener, router).await.unwrap()
+}
+
+pub fn build_router() -> Router {
+    Router::new().route("/wiki/", get(root))
+}
+
+pub async fn root() -> String {
+    "Hello wiki!".into()
 }
